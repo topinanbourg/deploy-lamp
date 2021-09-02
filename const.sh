ce093a8aca94c146855905e5692b36d951e566bd
JosNo <josselin.nauche@gmail.com>
2021-09-02 18:47:33 +0000

    use mamouth for next tests

--- const.sh
+++ const.sh
@@ -16,11 +16,11 @@ displayWithColor=true
 defaultHostName="s3.kalak.xyz"
 
 # Git repo name, and subfolder name (to deploy, backups..)
-repoName=deploy
+repoName="mamouth"
 
 # doit on ajouter des clefs ssh
 declare -a sshKeysToAdd=(
-	"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQCU38/eDLfHkQAYZKbQynOi+w1epcacid+jfy5oCTHPVPk51Kig3KDzF+gfOcXjzBZI15kyg/vBYSeHdKZHPqnyoiK17d/n4NaSlMXULR75lufGuQTgospJpcxtd4oI58n2i0hHtrueNaO/Xk97cngm+VQPmpjHx/3K3Gny0WAXUQ== JosNo2@TopMove"
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQCU38/eDLfHkQAYZKbQynOi+w1epcacid+jfy5oCTHPVPk51Kig3KDzF+gfOcXjzBZI15kyg/vBYSeHdKZHPqnyoiK17d/n4NaSlMXULR75lufGuQTgospJpcxtd4oI58n2i0hHtrueNaO/Xk97cngm+VQPmpjHx/3K3Gny0WAXUQ== JosNo2@TopMove"
 )
 
 # BDD cnx (will be overided to set "DB_HOST", ...)
