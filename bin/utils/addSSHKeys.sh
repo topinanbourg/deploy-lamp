0102d149b14d7e8ec8cc77f4980a96585c8eb56d
JosNo <josselin.nauche@gmail.com>
2021-09-02 15:44:06 +0000

    add ssh key only if not present in file

--- bin/utils/addSSHKeys.sh
+++ bin/utils/addSSHKeys.sh
@@ -7,9 +7,43 @@ else
     # ajout de clef parmis celle ayant le droit de se connecter au server
     echo "Add ssh keys (to allow connection to server).."
 
+    existingKey=0
+    addedKey=0
+    fileToCheck="/home/debian/.ssh/authorized_keys"
+
     for sshKey in "${sshKeysToAdd[@]}"; do
-       echo "$sshKey" >> /home/debian/.ssh/authorized_keys
-       echo "$sshKey" >> /root/.ssh/authorized_keys
+
+        # extract comment from key
+        # 
+        # ssh-rsa AAAAB3N...AXUQ== User@Machine
+        # ->                       User@Machine
+        commentKey=$(echo $sshKey | cut -d' ' -f3)
+
+        # is these comment already in file
+        grepReturn=$(grep "$commentKey" "$fileToCheck")
+
+        if [ "$grepReturn" == "" ]; then 
+            (( addedKey++ ))
+            echo "$sshKey" >> /home/debian/.ssh/authorized_keys
+            displayMsg "  \e[92m"$commentKey"\e[0m added."
+        else
+            (( existingKey++ ))
+            displayMsg "  \e[93m"$commentKey"\e[0m already present."
+        fi
     done
-    displayMsg " \e[92m"${#sshKeysToAdd[@]}"\e[0m ssh key added."
+
+    # display summarize
+    if [ $existingKey -gt 0 ]; then
+        displayMsg "  \e[92m"$existingKey"\e[0m ssh key already in place."
+    fi
+
+    if [ $addedKey -eq 0 ]; then
+        displayMsg "  \e[93mNo\e[0m ssh key to add."
+    else
+        displayMsg "  \e[92m"$addedKey"\e[0m ssh key added."
+    fi
+
+    displayMsg "Copy authorized_keys from \e[93mdebian\e[0m to \e[92mroot\e[0m user."
+    cp /home/debian/.ssh/authorized_keys /root/.ssh/authorized_keys
+    chown -R root:root /root/.ssh
 fi
