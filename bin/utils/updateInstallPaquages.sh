77534e249041eab5041e14a86e5b83ba7586ba6b
JosNo <josselin.nauche@gmail.com>
2021-09-02 14:39:11 +0000

    update installed package for a LAMP stack

--- bin/utils/updateInstallPaquages.sh
+++ bin/utils/updateInstallPaquages.sh
@@ -7,16 +7,44 @@ read -p "so, take a cofee, and press [Enter] when ready..."
 clear
 
 # reccup de la maj des paquets
-dnf update -y
+displayMsg "\e[93mapt update\e[0m"
+apt update
 
+displayMsg "Install \e[92mcurl\e[0m & \e[92mnano\e[0m"
 # install needed packages
-dnf install -y curl
-dnf install -y nano
+apt install -y curl
+apt install -y nano
+
+# install apache
+displayMsg "Install \e[92mApache\e[0m"
+apt install -y apache2
+
+# install firewall
+displayMsg "Install firewall \e[92mufw\e[0m"
+apt install -y ufw
+displayMsg "  Allow \e[92mSSH\e[0m ports 21"
+ufw allow in "SSH"
+displayMsg "  Allow \e[92mWWW Full\e[0m ports 80,443"
+ufw allow in "WWW Full"
+
+# install mariaDB
+displayMsg "Install \e[92mMariaDB\e[0m"
+apt install -y mariadb-server
+# secure install
+mysql_secure_installation
+
+# install PHP
+displayMsg "Install \e[92mPHP\e[0m"
+apt install -y php libapache2-mod-php php-mysql
+
+# install certbot for let's encrypt
+displayMsg "Install \e[92mCertbot\e[0m for let's encript"
+apt install python-certbot-apache
 
 # install de git
-dnf install -y git
+displayMsg "Install \e[92mgit\e[0m"
+apt install -y git
 
-clear
 echo ""
 echo "Operatin system up to date."
 echo ""
@@ -24,5 +52,4 @@ echo ""
 git --version
 
 read -p "Press [Enter] when ready..."
-clear
 
