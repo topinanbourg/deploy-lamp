a4778b5d096ca804fc65f978b16094e349d77aa3
JosNo <josselin.nauche@gmail.com>
2021-09-03 15:31:01 +0000

    install node, npm .. and finish with MariaDB

--- bin/utils/updateInstallPaquages.sh
+++ bin/utils/updateInstallPaquages.sh
@@ -9,10 +9,11 @@ read -p "so, take a cofee, and press [Enter] when ready..."
 displayMsg "\e[93mapt update\e[0m"
 apt update
 
-displayMsg "Install \e[92mcurl\e[0m & \e[92mnano\e[0m"
+displayMsg "Install \e[92mcurl\e[0m, \e[92mwget\e[0m & \e[92munzip\e[0m..."
 # install needed packages
 apt install -y curl
 apt install -y nano
+apt install -y wget unzip
 
 # install apache
 displayMsg "Install \e[92mApache\e[0m"
@@ -34,7 +35,14 @@ apt install -y fail2ban
 
 # install PHP
 displayMsg "Install \e[92mPHP\e[0m"
-apt install -y php libapache2-mod-php php-mysql
+apt install -y php libapache2-mod-php php-mysql php-cli php-zip
+
+# install Composer
+displayMsg "Install \e[92mComposer\e[0m"
+wget -O composer-setup.php https://getcomposer.org/installer
+php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+rm composer-setup.php
+
 
 # install certbot for let's encrypt
 displayMsg "Install \e[92mCertbot\e[0m for let's encript"
@@ -43,6 +51,20 @@ apt install -y python-certbot-apache
 # install de git
 displayMsg "Install \e[92mgit\e[0m"
 apt install -y git
+
+# install de NodeJs
+displayMsg "Install \e[92mNodeJs\e[0m"
+apt install -y nodejs
+
+# install de npm
+displayMsg "Install \e[92mnpm\e[0m"
+apt install -y npm
+
+# install mariaDB
+displayMsg "Install \e[92mMariaDB\e[0m"
+apt install -y mariadb-server
+# secure install
+mysql_secure_installation
 
 echo ""
 echo "Operatin system up to date."
