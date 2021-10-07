1056e8fd1cea3be4800094eedab5c5d937f91c1d
JosNo <josselin.nauche@gmail.com>
2021-10-07 17:26:08 +0200

    ajout de la clef ssh de mon PC (quand-meme)

--- const.sh
+++ const.sh
@@ -21,6 +21,7 @@ repoName="mamouth"
 # doit on ajouter des clefs ssh
 declare -a sshKeysToAdd=(
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAAAgQCU38/eDLfHkQAYZKbQynOi+w1epcacid+jfy5oCTHPVPk51Kig3KDzF+gfOcXjzBZI15kyg/vBYSeHdKZHPqnyoiK17d/n4NaSlMXULR75lufGuQTgospJpcxtd4oI58n2i0hHtrueNaO/Xk97cngm+VQPmpjHx/3K3Gny0WAXUQ== JosNo2@TopMove"
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC/FYidiAvdx8Rw8LzJz4DcfRni4ngm70fRfcDLE8UUoMSnIwC+PrzbLou7cf40UUWfwph8mLjQdjLWflpTuqC72+ar4PkVl2/Z9s/+74MVtO0dYI7oWJLUpw3cooOVh4Qc67QqxLw8nr7qUxiGNYuiiaw8fYJ0kXbbihU8VrCBhZfui7J5sWWIC7pe3lgnscp9O0A4Y9RyHfEJcIcQaVEJF+x5bwMCxKTMoiLQuUhPubdYN14jov8AO2xgPJjJPF7wkeKEthbyySX0A3kA1m9ZvKTIiNxnRaKs13kvWUNZwmUrqJT3jHDJE0E1EDmuzCYMULJyZclPEIO2KNTvbLq9NkxBasX1zpneEe0lyXnMGenCmE0JYGsNqGEsciE6O8QhYapPGQQKtxACrlnImQWbvthjI80iICzRyMcwMHUJ9QkuvF1vOEE8Y4dMEdThyLICz//Syoaqv80sBvlHtxuES8R3yHUOm0PGuAKTGQXUdMT9QPszfkSFgr4uUu41gwiJnqytsZ7jRx3F91FcMgutrYGX+OVArCmvyzooFmawyX4Vka/4YKfLy/uFqo4HRFWRcZJ6vWc/l57c9+etlHyFxXyGd6RkkZDGWkUhIhUZxWQv5Kb5u+7Uzn+gIiJMVuNU4EprHNal06+t+P1+IicjAbjzzB6l7GDYQRumm8vEKQ== josselin.nauche@gmail.com"
 )
 
 # BDD cnx (will be overided to set "DB_HOST", ...)
